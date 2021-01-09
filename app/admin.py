faeb87b75708860b6d159dfdd83aa8517280b297
josieesh <jeshkena@uwaterloo.ca>
2021-01-09 18:28:54 -0500

    hello again

--- app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Textfile
-
-@admin.register(Textfile)
-class TextfileAdmin(admin.ModelAdmin):
-    list_display = ('name', 'text', 'parent_page','heading_level','order')
-    fields = ('page','heading_level','tags','name','text','parent_page','order')
