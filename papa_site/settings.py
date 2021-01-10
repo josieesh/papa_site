196adb4ef1d44d32efc89ef74e037839e3c2fb35
josieesh <jeshkena@uwaterloo.ca>
2021-01-10 13:31:26 -0500

    comment out again???

--- papa_site/settings.py
+++ papa_site/settings.py
@@ -134,9 +134,9 @@ STATICFILES_DIRS = [
 ]
 
 
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+# import dj_database_url
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
 
 import django_heroku
 # Activate Django-Heroku.
