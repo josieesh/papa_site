a58162df3291cff4ddf29af99f5e2a95d0249c67
josieesh <jeshkena@uwaterloo.ca>
2021-01-10 12:44:00 -0500

    procfile +gunicron

--- papa_site/settings.py
+++ papa_site/settings.py
@@ -127,13 +127,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+#STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
 
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+# import dj_database_url
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
+
+import django_heroku
+# Activate Django-Heroku.
+django_heroku.settings(locals())
