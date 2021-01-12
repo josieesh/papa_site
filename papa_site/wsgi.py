cdd4abb9ab80b9b32b507682dd62615899eb496c
josieesh <jeshkena@uwaterloo.ca>
2021-01-12 17:53:14 -0500

    newline and tables support

--- papa_site/wsgi.py
+++ papa_site/wsgi.py
@@ -13,6 +13,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'papa_site.settings')
 
-print("GETTING WSGI APPLICATION")
 application = get_wsgi_application()
-print("GOT WSGI APPLICATION")
