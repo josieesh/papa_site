5e36d64922f845e59db239e348434fa328821be1
josieesh <jeshkena@uwaterloo.ca>
2021-08-23 00:23:48 -0700

    New models, wip

--- app/admin.py
+++ app/admin.py
@@ -1,8 +1,38 @@
 from django.contrib import admin	
 
-from .models import Textfile	
+from .models import Textfile, Heading1, Heading2, Page
 
 @admin.register(Textfile)	
 class TextfileAdmin(admin.ModelAdmin):	
     list_display = ('name', 'parent_page','heading_level','order', 'is_table')	
     fields = ('page','heading_level','tags','name','text','parent_page','order', 'is_table')
+
+
+"""
+Inlines
+"""
+class Heading1Inline(admin.TabularInline):
+    model = Heading1
+
+
+
+class Heading2Inline(admin.TabularInline):
+    model = Heading2
+
+
+
+"""
+ModelAdmins
+"""
+@admin.register(Page)	
+class PageAdmin(admin.ModelAdmin):	
+    inlines = [
+        Heading1Inline,
+    ]
+
+@admin.register(Heading1)	
+class Heading1Admin(admin.ModelAdmin):	
+    inlines = [
+        Heading2Inline,
+    ]
+
--- app/models.py
+++ app/models.py
@@ -2,11 +2,19 @@ from django.db import models
 from datetime import datetime
 import uuid
 from django.contrib import admin
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+
+class Page(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+    
 
 class Textfile(models.Model):
     page = models.CharField(max_length=100)
     heading_level = models.IntegerField(default=1)
-    tags = models.CharField(max_length=255, default=None, blank=True, null=True)
     name = models.CharField(max_length=255, blank=True)
     text = models.TextField(blank=True, null=True)
     is_table = models.BooleanField(default=False)
@@ -20,5 +28,46 @@ class Textfile(models.Model):
     def __str__(self):
         display = "Page: " + self.page + ", Title: " + self.name + ", Order: " + str(self.order) + ", Table: " + str(self.is_table)
         return display
+
+
+class Heading1(models.Model):
+    page = models.ForeignKey(Page, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, blank=True)
+    text = models.TextField(blank=True, null=True)
+    order = models.IntegerField(default=1)
+    date_added = models.DateTimeField(default=datetime.now, editable=False)
+    
+
+    exclude = ["date_added"]
+
+    def __str__(self):
+        display = "Name: " + self.name + ", Page: " + self.page.name
+        return display
+
+
+class Heading2(models.Model):
+    parent_heading = models.ForeignKey(Heading1, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, blank=True)
+
+    # def __str__(self):
+    #     display = "Page: " + self.page + ", Name: " + self.name
+    #     return display
+
+
+class Table(models.Model):
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    parent_heading = GenericForeignKey('content_type', 'object_id')
+
+
+"""
+heading = Heading1.objects.get(name='Blahblah')
+t = Table(parent_heading=heading)
+t.save()
+t.content_object
+
+<Heading1: Blahblah>
+"""
+
 
 
