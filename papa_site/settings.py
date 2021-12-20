9402cecfa7cb093ad5b56bb9c8f59196f5fffd8c
josieesh <jeshkena@uwaterloo.ca>
2021-12-19 20:46:38 -0500

    AGSDJKLFSJKAHDJHDJF

--- papa_site/settings.py
+++ papa_site/settings.py
@@ -118,10 +118,7 @@ WSGI_APPLICATION = 'papa_site.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+
 }
 
 
